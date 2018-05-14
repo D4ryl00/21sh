a2a7eb66eba42d5659e5f5f3ddcb962b049079b6
Remi BARBERO <rbarbero@student.42.fr>
2018-05-14 16:50:00 +0200

    fix infinite loop in ft_buf_delete_char

--- libft/ft_buf_delete_char.c
+++ libft/ft_buf_delete_char.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/14 14:55:24 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/05/14 15:06:11 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/05/14 16:49:24 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	ft_buf_delete_char(t_buf *buffer, int index)
 	data_len = 1;
 	if (index >= buffer->i)
 		return (0);
-	i = index;
-	while (i < buffer->i - 1)
+	i = index - 1;
+	while (++i < buffer->i - 1)
 		buffer->buf[i] = buffer->buf[i + 1];
 	(buffer->i)--;
 	return (1);
