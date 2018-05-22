80b618989ea0e21c201cf44fa2752aceed660ccb
d4ryl00 <d4ryl00@gmail.com>
2018-05-22 12:52:38 +0200

    add io_number classification

--- srcs/classify_token.c
+++ srcs/classify_token.c
@@ -6,14 +6,14 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/19 13:26:30 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/05/22 10:32:48 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/05/22 11:03:44 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "sh.h"
 
-void	classify_token_operator(t_list *tokens)
+void	classify_operator_token(t_list *tokens)
 {
 	int	i;
 
@@ -33,8 +33,37 @@ void	classify_token_operator(t_list *tokens)
 	}
 }
 
+void	classify_ionumber_token(t_list *tokens)
+{
+	char	*str;
+
+	while (tokens)
+	{
+		str = ((t_token *)tokens->content)->content;
+		if (*str == '<' || *str == '>')
+		{
+			while (*str)
+			{
+				if (!ft_isdigit(*(str++)))
+					break ;
+			}
+			if (!(*str))
+				((t_token *)tokens->content)->type = IO_NUMBER;
+		}
+		else if (ft_isdigit(*str))
+		{
+			while (ft_isdigit(*str))
+				str++;
+			if ((*str == '>' || *str == '<') && !str[1])
+				((t_token *)tokens->content)->type = IO_NUMBER;
+		}
+		tokens = tokens->next;
+	}
+}
+
 int	classify_token(t_list *tokens)
 {
-	classify_token_operator(tokens);
+	classify_operator_token(tokens);
+	classify_ionumber_token(tokens);
 	return (0);
 }
