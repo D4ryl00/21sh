8607cd989dfc671980b6a961f6cabed87630e4bf
Remi BARBERO <rbarbero@student.42.fr>
2018-09-17 16:41:39 +0200

    fix bug in ft_strarrchr

--- libft/ft_strarrchr.c
+++ libft/ft_strarrchr.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 16:01:14 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/09/17 16:07:42 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/09/17 16:29:11 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_strarrchr(char *str, char **arr)
 	i = -1;
 	if (arr)
 	{
-		while (arr[++i])
+		while (arr[++i] && arr[i][0])
 		{
 			if (!ft_strcmp(arr[i], str))
 				return (i);
