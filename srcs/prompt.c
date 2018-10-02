164201c943e72cc94f3355ac82da47d5b3d6e14a
Adrien MORDRET <amordret@student.42.fr>
2018-10-02 09:15:02 +0200

    a

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: amordret <amordret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 13:48:23 by amordret          #+#    #+#             */
-/*   Updated: 2018/09/17 16:44:56 by amordret         ###   ########.fr       */
+/*   Updated: 2018/09/25 11:53:43 by amordret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,23 @@
 static void	sh_print_prompt(void)
 {
 	char	*pwd;
-
-	if (!(((pwd = getenv("PWD")) == NULL)))
+	
+	pwd = NULL;
+	/*if (!(((pwd = getenv("PWD")) == NULL)))
 	{
-		ft_putstr(PROMPT1);
-		ft_putstr(pwd);
+		term_putstr(PROMPT1);
+		term_putstr(pwd);
 	}
-	ft_putstr(PROMPT2);
-	ft_putstr(PROMPT3);
+	term_putstr(PROMPT2);
+	term_putstr(PROMPT3);*/
+	term_putstr("> ");
 	g_termcaps.writtenchars = 0;
 }
 
 void		prompt(char *promptstring)
 {
 	if (promptstring)
-		ft_putstr(promptstring);
+		term_putstr(promptstring);
 	else
 		sh_print_prompt();
 }
