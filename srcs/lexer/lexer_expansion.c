eb0804b4d24ac123ba292c82836e12784635cf7f
d4ryl00 <d4ryl00@gmail.com>
2018-10-06 18:27:23 +0200

    cleanup expansion lexer

--- srcs/lexer/lexer_expansion.c
+++ srcs/lexer/lexer_expansion.c
@@ -6,17 +6,31 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/18 13:54:19 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/05/18 14:05:45 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/10/06 18:24:52 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "sh.h"
 
+static int	variable_case(t_buf *buffer, t_input *input
+		, unsigned char f_params[2])
+{
+	(void)f_params;
+	while (*(input->str) && *(input->str) != ' ' && *(input->str) != '\n')
+	{
+		if (ft_buf_add_char(buffer, *(input->str)) == -1)
+			exit_perror(ENOMEM, NULL);
+		(input->str)++;
+	}
+	return (0);
+}
+
 int	dollar_case(t_buf *buffer, t_input *input
 		, unsigned char f_params[2])
 {
-	ft_buf_add_char(buffer, *(input->str));
+	if (ft_buf_add_char(buffer, *(input->str)) == -1)
+		exit_perror(ENOMEM, NULL);
 	(input->str)++;
 	if (*(input->str) == '{')
 		get_token_expansion(buffer, input, f_params);
@@ -24,6 +38,8 @@ int	dollar_case(t_buf *buffer, t_input *input
 		get_token_arithmetic(buffer, input, f_params);
 	else if (*(input->str) == '(')
 		substitution_case(buffer, input, f_params, ')');
+	else
+		variable_case(buffer, input, f_params);
 	f_params[0] = 1;
 	return (0);
 }
@@ -37,10 +53,8 @@ int	get_token_expansion(t_buf *buffer, t_input *input
 		{
 			free(input->save);
 			if (!newprompt(input, "> "))
-				exit_perror(EOTHER, "syntax error");
+				exit_perror(ENOMEM, NULL);
 		}
-		if (*(input->str) == '}')
-			break ;
 		else if (*(input->str) == '\'')
 			sq_case(buffer, input, f_params);
 		else if (*(input->str) == '"')
@@ -49,11 +63,12 @@ int	get_token_expansion(t_buf *buffer, t_input *input
 			bs_case(buffer, input, f_params);
 		else
 		{
-			ft_buf_add_char(buffer, *(input->str));
+			if (ft_buf_add_char(buffer, *(input->str)) == -1)
+				exit_perror(ENOMEM, NULL);
 			(input->str)++;
+			if (*(input->str - 1) == '}')
+				break ;
 		}
 	}
-	ft_buf_add_char(buffer, *(input->str));
-	(input->str)++;
 	return (0);
 }
