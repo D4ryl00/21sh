b2cabbfa3556b8591c7944bc0625e242d5878440
D4ryl00 <d4ryl00@gmail.com>
2019-09-18 23:59:17 +0200

    utilities and builtins commands must quit in multipipes

--- srcs/eval/builtin_cmd.c
+++ srcs/eval/builtin_cmd.c
@@ -6,7 +6,7 @@
 /*   By: amordret <amordret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/05 13:26:50 by rbarbero          #+#    #+#             */
-/*   Updated: 2019/09/16 21:10:19 by rbarbero         ###   ########.fr       */
+/*   Updated: 2019/09/18 23:54:23 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,17 @@ int		run_builtin_cmd(char **av)
 			|| (g_jobctrl.current_job->forked && g_jobctrl.current_job->child))
 	{
 		if (!ft_strcmp(av[0], "echo"))
-			return(builtin_echo(av));
+			status = builtin_echo(av);
 		else if (!ft_strcmp(av[0], "env"))
-			return(builtin_env(av));
+			status = builtin_env(av);
 		else if (!ft_strcmp(av[0], "setenv"))
-			return(builtin_setenv(&g_env, av));
+			status = builtin_setenv(&g_env, av);
 		else if (!ft_strcmp(av[0], "unsetenv"))
-			return(builtin_unsetenv(&g_env, av));
+			status = builtin_unsetenv(&g_env, av);
 		else if (!ft_strcmp(av[0], "exit"))
 			builtin_exit(av);
+		if (g_jobctrl.current_job->child)
+			exit(status);
 	}
 	else if (!g_jobctrl.current_job->nowait && !g_jobctrl.current_job->async)
 		status = waitjob(g_jobctrl.current_job);
--- srcs/eval/utility_cmd.c
+++ srcs/eval/utility_cmd.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/05 13:29:58 by rbarbero          #+#    #+#             */
-/*   Updated: 2019/09/17 22:58:46 by rbarbero         ###   ########.fr       */
+/*   Updated: 2019/09/18 23:57:00 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,13 @@ int		run_utility_cmd(char **av)
 			|| (g_jobctrl.current_job->forked && g_jobctrl.current_job->child))
 	{
 		if (!ft_strcmp(av[0], "cd"))
-			return(utility_cd(av));
+			status = utility_cd(av);
 		else if (!ft_strcmp(av[0], "fg"))
-			return(utility_fg(av));
+			status = utility_fg(av);
 		else if (!ft_strcmp(av[0], "jobs"))
-			return(utility_jobs(av));
+			status = utility_jobs(av);
+		if (g_jobctrl.current_job->child)
+			exit(status);
 	}
 	else if (!g_jobctrl.current_job->nowait && !g_jobctrl.current_job->async)
 		status = waitjob(g_jobctrl.current_job);
